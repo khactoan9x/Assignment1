bf8b9b75187ec8bfcabab3b9d039ac5d49eead76
Phuc Nguyen <kufuc2604@gmail.com>
2019-02-25 22:06:21 -0500

    Fixed create player module

--- src/java/logic/UsernameLogic.java
+++ src/java/logic/UsernameLogic.java
@@ -1,6 +1,7 @@
 package logic;
 
 import dao.UsernameDAO;
+import entity.Player;
 import entity.Username;
 import java.util.List;
 import java.util.Map;
@@ -9,35 +10,39 @@ import java.util.Map;
  *
  * @author Shariar
  */
-public class UsernameLogic extends GenericLogic<Username,UsernameDAO>{
-    
+public class UsernameLogic extends GenericLogic<Username, UsernameDAO> {
+
     public static final String PLAYER_ID = "id";
     public static final String USERNAME = "username";
-  
-    public UsernameLogic() { 
-        super( new UsernameDAO());
-        
+
+    public UsernameLogic() {
+        super(new UsernameDAO());
+
     }
-    
-    public List<Username> getAll(){
-        
-        return get(()->dao().findAll());
+
+    public List<Username> getAll() {
+
+        return get(() -> dao().findAll());
     }
-    
-    public Username getByID( int playerID){
-        return get(()->dao().findByPlayerID(playerID));
+
+    public Username getByID(int playerID) {
+        return get(() -> dao().findByPlayerID(playerID));
     }
-    
-    public List<Username> getByUserName( String username){
-        return get(()->dao().findByUsername(username));
+
+    public List<Username> getByUserName(String username) {
+        return get(() -> dao().findByUsername(username));
     }
 
     public Username createEntity(Map<String, String[]> parameterMap) {
         Username user = new Username();
-        if(parameterMap.containsKey(PLAYER_ID)){
-            user.setPlayerid(Integer.valueOf(parameterMap.get(PLAYER_ID)[0]));
-        }
+        PlayerLogic playerLogic = new PlayerLogic();
+        System.out.println(parameterMap.get(PLAYER_ID)[0]);
+        System.out.println(parameterMap.get(USERNAME)[0]);
+        Player player = playerLogic.getPlayerWithId(Integer.valueOf(parameterMap.get(PLAYER_ID)[0]));
+        user.setPlayer(player);
+        user.setPlayerid(Integer.valueOf(parameterMap.get(PLAYER_ID)[0]));
         user.setUsername(parameterMap.get(USERNAME)[0]);
+        System.out.println("created");
         return user;
     }
 }
